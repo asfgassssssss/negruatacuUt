ac6fa7b5b02002a02285ca31ae4aa543bf65e6a3
iliy <cyganyty@gmail.com>
2023-11-05 19:01:54 +0300

    ghjkl;'lkjhgfxv

---
+++ negr/XJIe6/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from XJIe6.views import index, archive, SFASFGAS, sdghsdfh, kub
+from . import views
+urlpatterns = [
+    path('kub/',kub),
+    path('XJIe6/', index),
+    path('', views.index),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.sdghsdfh),
+    path('archive/<int:years>/',archive),
+]
---
+++ negr/negr/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for negr project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from XJIe6.views import page_not_found, page, page403
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('XJIe6.urls')),
+]
+handler404 = page_not_found
+handler500 = page
+handler403 = page403
